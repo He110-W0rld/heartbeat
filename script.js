1ec6cd0f631266d440de4bc768b93e96bf8365fd
Vitalii Vorontsov <vvorontsov@amcbridge.com>
2025-02-22 16:02:37 +0200

    add website

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const heart = document.querySelector(".heart");
+  
+    const socket = io("http://35.176.160.64:3000");
+  
+    socket.on('pulse', () => {
+      heart.classList.add("pulse");
+  
+      setTimeout(() => heart.classList.remove("pulse"), 800);
+    });
+  
+    heart.addEventListener("click", (event) => {
+      event.stopPropagation();
+  
+      heart.classList.add("pulse");
+  
+      socket.emit('pulse');
+  
+      setTimeout(() => heart.classList.remove("pulse"), 800);
+    });
+  });
+  
