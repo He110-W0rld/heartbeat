8b8d057eb6044ef1ab1fd0011e34d82a5e7fa748
He110-W0rld <131450226+He110-W0rld@users.noreply.github.com>
2025-02-23 00:26:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const heart = document.querySelector(".heart");
-  
-    const socket = io("http://00.00.00.00:3000");
-  
-    socket.on('pulse', () => {
-      heart.classList.add("pulse");
-  
-      setTimeout(() => heart.classList.remove("pulse"), 800);
-    });
-  
-    heart.addEventListener("click", (event) => {
-      event.stopPropagation();
-  
-      heart.classList.add("pulse");
-  
-      socket.emit('pulse');
-  
-      setTimeout(() => heart.classList.remove("pulse"), 800);
-    });
+  const heart = document.querySelector(".heart");
+  const socket = io("https://heartbeat.backend.website");
+
+  socket.on('pulse', () => {
+
+    heart.classList.add("pulse");
+
+
+    setTimeout(() => heart.classList.remove("pulse"), 800);
   });
-  
+
+
+  heart.addEventListener("click", (event) => {
+    event.stopPropagation();
+
+
+    heart.classList.add("pulse");
+
+    socket.emit('pulse');
+
+    setTimeout(() => heart.classList.remove("pulse"), 800);
+  });
+});
