9013413cd9c80590b6c6bf8d79eecdd32c844540
He110-W0rld <131450226+He110-W0rld@users.noreply.github.com>
2025-03-14 21:21:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const heart = document.querySelector(".heart");
-  
-    // const socket = io("http://00.00.00.00:3000");
-    const socket = io("https://heartbeat.backend.website");
-  
-    socket.on('pulse', () => {
-      heart.classList.add("pulse");
-  
-      setTimeout(() => heart.classList.remove("pulse"), 800);
-    });
-  
-    heart.addEventListener("click", (event) => {
-      event.stopPropagation();
-  
-      heart.classList.add("pulse");
-  
-      socket.emit('pulse');
-  
-      setTimeout(() => heart.classList.remove("pulse"), 800);
-    });
+  const heart = document.querySelector(".heart");
+
+  // const socket = io("http://00.00.00.00:3000");
+  const socket = io("https://heartbeat.backend.website");
+
+  socket.on("pulse", () => {
+    heart.classList.add("pulse");
+    setTimeout(() => heart.classList.remove("pulse"), 800);
   });
+
+  heart.addEventListener("click", (event) => {
+    event.stopPropagation();
+
+    heart.classList.add("pulse");
+
+    socket.emit("pulse");
+
+    fetch("http://35.179.115.82:5000/notify", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: "❤️" }),
+    })
+    .then(response => response.text())
+    .then(data => console.log("Telegram sending:", data))
+    .catch(error => console.error("Sending error:", error));
+
+    setTimeout(() => heart.classList.remove("pulse"), 800);
+  });
+});
